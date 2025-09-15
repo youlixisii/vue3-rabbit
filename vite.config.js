40e948def9ee0cca7fc60a9a11da26ec6f7ccd28
friday <1172147378@qq.com>
2025-09-15 23:30:52 +0800

    element主题色定制

--- vite.config.js
+++ vite.config.js
@@ -14,10 +14,29 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers:[ElementPlusResolver()],
+    }),
+    Components({
+      resolvers:[
+        ElementPlusResolver({importStyle:"sass"}),
+
+      ],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
