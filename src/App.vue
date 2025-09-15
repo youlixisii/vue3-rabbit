889f248a5e1cad338e6ab6fe1d0232397d8143b8
friday <1172147378@qq.com>
2025-09-16 00:33:54 +0800

    scss文件自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
 <template>
   <!-- 一级路由出口组件 -->
   <RouterView />
+  <div class="test">
+    test
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+  .test{
+    color:$priceColor;
+  }
+
+</style>
