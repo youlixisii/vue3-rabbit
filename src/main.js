574801afd882388d8bfd4d13d3d5696d91f1b746
friday <1172147378@qq.com>
2025-09-16 00:25:27 +0800

    静态资源引入和插件安装

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import '@/styles/common.scss'  //引入样式
 
 import {getCategoryAPI} from '@/apis/testAPI'
 getCategoryAPI().then(res=>{
