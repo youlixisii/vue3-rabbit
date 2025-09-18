8cc3ced456222645c9009bb2506e7d82f5b490f6
friday <1172147378@qq.com>
2025-09-18 22:24:59 +0800

    图片懒加载

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,25 @@ export function getBannerAPI(){
     url:'/home/banner'
   })
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return http({
+    url:'/home/new'
+  })
+}
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+  return http({
+    url:'/home/hot'
+  })
+}
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 import '@/styles/common.scss'  //引入样式
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 
 const app = createApp(App)
 
@@ -12,3 +14,20 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.directive('img-lazy',{
+  mounted(el,binding){
+    console.log(el,binding.value);
+    const { stop } = useIntersectionObserver(
+          el,
+          ([{ isIntersecting }]) => {
+            console.log(isIntersecting)
+            if (isIntersecting) {
+              // 进入视口区域
+              el.src = binding.value
+              stop()
+            }
+          },
+        )
+  }
+})
