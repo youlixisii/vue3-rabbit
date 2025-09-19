e9429dacdbc35ff5afb09c25d0c11fe605fc96b7
friday <1172147378@qq.com>
2025-09-20 00:23:42 +0800

    二级分类商品列表

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { getCategoryFilterAPI } from '@/apis/category';
+import { getCategoryFilterAPI,getSubCategoryAPI } from '@/apis/category';
 import {onMounted, ref} from 'vue'
 import { useRoute } from 'vue-router';
+import Goodsitem from '../Home/components/Goodsitem.vue';
 
 //获取面包屑导航数据
 const categoryData=ref({})
@@ -11,6 +12,22 @@ const getCategoryData= async ()=>{
   categoryData.value=res.result
 }
 onMounted(()=>getCategoryData())
+
+//基础列表数据
+const goodList=ref([])
+const reqData=ref({
+  categoryId:route.params.id,
+  page:1,
+  pageSize:20,
+  sortField:'publishTime'
+})
+const getGoodList =async ()=>{
+  const res=await getSubCategoryAPI(reqData.value)
+  console.log(res);
+  goodList.value=res.result.items
+
+}
+onMounted(()=>getGoodList())
 
 </script>
 
@@ -35,7 +52,8 @@ onMounted(()=>getCategoryData())
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
       <div class="body">
-         <!-- 商品列表-->
+          <!-- 商品列表-->
+        <Goodsitem v-for="good in goodList" :goods="good" :key="good.id" />
       </div>
     </div>
   </div>
