841012dc5c0296a614118b88e51bcbaa265003bf
friday <1172147378@qq.com>
2025-09-21 21:09:55 +0800

    详情页图片放大镜

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import {getDetail} from '@/apis/detail'
 import {onMounted, ref} from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView/index.vue'
 
 //获取商品详情页数据
 const goods=ref({})
@@ -41,7 +42,7 @@ onMounted(()=>getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView :image-list="goods.mainPictures"/>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
