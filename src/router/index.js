fa87bd92aab7d22da44e0b6017b33cc9fce5b5e7
friday <1172147378@qq.com>
2025-10-03 13:06:54 +0800

    会员中心-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 //2.创建路由器  调用createRouter
 //路由器里面管理一个个路由，所以是routes，它是一个数组[]，而路由又是一个个对应关系，所以写{}
@@ -66,6 +69,20 @@ const router = createRouter({
         {
           path:'paycallback',
           component:PayBack
+        },
+        {
+          path:'member',
+          component:Member,
+          children:[{
+            path:'user',
+            component:UserInfo
+          },
+          {
+            path:'order',
+            component:UserOrder
+          }
+
+        ]
         }
       ]
     },
